917c73d72eedb10dae6c6101284f1da81695fd7c
UniqueUdit <39905041+UniqueUdit@users.noreply.github.com>
2021-03-22 12:46:44 +0530

    Add files via upload

---
+++ NewAccountAgain.java
@@ -0,0 +1,101 @@
+package com.banking.testCases;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+
+import org.openqa.selenium.By;
+import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
+
+import com.banking.pageObject.LoginPage;
+import com.banking.pageObject.NewAccount;
+
+public class NewAccountAgain extends BaseClass {
+	String data;
+@Test
+public void loginTest() throws Exception {
+
+ driver.get(baseUrl);
+ LoginPage lp=new LoginPage(driver);
+ lp.setUserName(username);
+ logger.info("User name is provided");
+ lp.setPassword(password);
+ logger.info("Passsword is provided");
+ lp.clickSubmit();
+ NewAccount cust = new NewAccount(driver);
+ cust.clickAddNewAccount();
+ try {
+     File myObj = new File("C:\\Users\\User8\\OneDrive - HCL Technologies Ltd\\Desktop\\Customer1.txt");
+     Scanner myReader = new Scanner(myObj);
+     while (myReader.hasNextLine()) {
+        data = myReader.nextLine();
+      
+     }
+     myReader.close();
+   } catch (FileNotFoundException e) {
+     System.out.println("An error occurred.");
+     e.printStackTrace();
+   }
+ cust.setCustomer(data);
+ logger.info("Customer id entered");
+ cust.setAccountType();
+ cust.setInitialdeposit("100000");
+ logger.info("initaial deposite entered");
+ cust.clickSubmit();
+ //String AccountNo = driver.findElement(By.xpath("//td[text()='90630']")).getText();
+ String tableTag = driver.findElement(By.id("account")).getTagName();
+  SoftAssert softAssert = new SoftAssert();
+ if(tableTag.contains("table")) {
+     softAssert.assertTrue(true);
+     System.out.println("table Present");
+ }
+ String titleTag = driver.findElement(By.className("heading3")).getText();
+ if(titleTag.contains("Account Generated Successfully!!!")) {
+     softAssert.assertTrue(true);
+     System.out.println("Generated Successfully");
+ }
+ String AccountID1 = driver.findElement(By.xpath("//table[@id='account']/tbody[1]/tr[4]/td[2]")).getText();
+ logger.info(AccountID1);
+ String CustomerID = driver.findElement(By.xpath("//table[@id='account']/tbody[1]/tr[5]/td[2]")).getText();
+ logger.info(CustomerID);
+ 
+ 
+ Boolean cont = driver.findElement(By.xpath("//table[@id='account']/tbody[1]/tr[11]/td[1]/a[1]")).isDisplayed();
+ System.out.println("Continue = "+cont);
+
+ Boolean home = driver.findElement(By.xpath("html[1]/body[1]/p[1]/a[1]")).isDisplayed();
+ System.out.println("Home = "+home);
+ File file = new File("C:\\Users\\User8\\OneDrive - HCL Technologies Ltd\\Desktop\\AccountId1s.txt"); //initialize File object and passing path as argument  
+ boolean result;  
+ try   
+ {  
+ result = file.createNewFile();  //creates a new file  
+ if(result)      // test if successfully created a new file  
+ {  
+ System.out.println("file created "+file.getCanonicalPath()); //returns the path string  
+ }  
+ else  
+ {  
+ System.out.println("File already exist at location: "+file.getCanonicalPath());  
+ }  
+ }   
+ catch (IOException e)   
+ {  
+ e.printStackTrace();    //prints exception if any  
+ } 
+ try 
+ {
+     FileWriter myWriter = new FileWriter("C:\\Users\\User8\\OneDrive - HCL Technologies Ltd\\Desktop\\AccountId1.txt");
+     myWriter.write(AccountID1);
+     myWriter.close();
+     }
+ catch (IOException e) {
+     System.out.println("An error occurred.");
+     e.printStackTrace();
+   }
+}
+
+}
