b28793f1a05fb1c03ea35f49d64916ba8d3b9220
UniqueUdit <39905041+UniqueUdit@users.noreply.github.com>
2021-03-26 09:37:04 +0530

    Update NewAccount.java

--- NewAccount.java
+++ NewAccount.java
@@ -1,61 +1,102 @@
-package com.banking.pageObject;
-import org.openqa.selenium.support.How;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.CacheLookup;
-import org.openqa.selenium.support.FindBy;
-import org.openqa.selenium.support.PageFactory;
-public class NewAccount {
-   
-    WebDriver driver;
-   
-    public NewAccount(WebDriver  rdriver)
-    {
-        driver=rdriver;
-        PageFactory.initElements(rdriver, this);
-       
-       
-    }
-    @FindBy(how = How.XPATH, using ="//a[@href='addAccount.php']")
-    @CacheLookup
-    WebElement lnkAddNewAccount;
-    @FindBy(name="cusid")
-    @CacheLookup
-    WebElement txtCustomer;
-   
-    @FindBy(name="selaccount")
-    @CacheLookup
-    WebElement txtAccountType;
-   
-    @FindBy(name="inideposit")
-    @CacheLookup
-    WebElement txtInitialdeposit;
-   
-    @FindBy(name="button2")
-    @CacheLookup
-    WebElement txtbtnLogin;
-   
-    public void clickAddNewAccount() {
-        lnkAddNewAccount.click();
-           
-    }
-   
-   
-    public void setCustomer(String uname)
-    {
-        txtCustomer.sendKeys(uname);
-    }
-   
-    public void setAccountType() {
-        txtAccountType.click();
-    }
-   
-    public void setInitialdeposit(String password)
-    {
-        txtInitialdeposit.sendKeys(password);
-    }
-    public void clickSubmit()
-    {
-        txtbtnLogin.click();
-    }
+package com.banking.testCases;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import org.testng.asserts.Assertion;
+ 
+import org.testng.asserts.SoftAssert;
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.By;
+import org.testng.Assert;
+
+import com.banking.pageObject.LoginPage;
+import com.banking.pageObject.NewAccount;
+public class TC_NewAccount_06 extends BaseClass {
+	String data;
+@Test
+public void loginTest() throws Exception {
+
+ driver.get(baseUrl);
+ LoginPage lp=new LoginPage(driver);
+ lp.setUserName(username);
+ logger.info("User name is provided");
+ lp.setPassword(password);
+ logger.info("Passsword is provided");
+ lp.clickSubmit();
+ NewAccount cust = new NewAccount(driver);
+ cust.clickAddNewAccount();
+ try {
+     File myObj = new File("C:\\Users\\User8\\OneDrive - HCL Technologies Ltd\\Desktop\\Customer1.txt");
+     Scanner myReader = new Scanner(myObj);
+     while (myReader.hasNextLine()) {
+        data = myReader.nextLine();
+      
+     }
+     myReader.close();
+   } catch (FileNotFoundException e) {
+     System.out.println("An error occurred.");
+     e.printStackTrace();
+   }
+ cust.setCustomer(data);
+ logger.info("Customer id entered");
+ cust.setAccountType();
+ cust.setInitialdeposit("100000");
+ logger.info("initaial deposite entered");
+ cust.clickSubmit();
+ //String AccountNo = driver.findElement(By.xpath("//td[text()='90630']")).getText();
+ String tableTag = driver.findElement(By.id("account")).getTagName();
+  SoftAssert softAssert = new SoftAssert();
+ if(tableTag.contains("table")) {
+     softAssert.assertTrue(true);
+     System.out.println("table Present");
+ }
+ String titleTag = driver.findElement(By.className("heading3")).getText();
+ if(titleTag.contains("Account Generated Successfully!!!")) {
+     softAssert.assertTrue(true);
+     System.out.println("Generated Successfully");
+ }
+ String AccountID = driver.findElement(By.xpath("//table[@id='account']/tbody[1]/tr[4]/td[2]")).getText();
+ logger.info(AccountID);
+ String CustomerID = driver.findElement(By.xpath("//table[@id='account']/tbody[1]/tr[5]/td[2]")).getText();
+ logger.info(CustomerID);
+ 
+ 
+ Boolean cont = driver.findElement(By.xpath("//table[@id='account']/tbody[1]/tr[11]/td[1]/a[1]")).isDisplayed();
+ System.out.println("Continue = "+cont);
+
+ Boolean home = driver.findElement(By.xpath("html[1]/body[1]/p[1]/a[1]")).isDisplayed();
+ System.out.println("Home = "+home);
+ try{
+	    
+	    File file = new File("C:\\Users\\User8\\OneDrive - HCL Technologies Ltd\\Desktop\\AccountId.txt");
+
+	    // if file doesnt exists, then create it
+	    if (!file.exists()) {
+	        file.createNewFile();
+	        FileWriter myWriter = new FileWriter("C:\\Users\\User8\\OneDrive - HCL Technologies Ltd\\Desktop\\AccountId.txt");
+	        myWriter.write(AccountID);
+	        myWriter.close();
+	    }else{
+
+	    }
+
+	    FileWriter fw = new FileWriter(file.getAbsoluteFile());
+	    BufferedWriter bw = new BufferedWriter(fw);
+	    bw.write(AccountID);
+	    bw.close();
+
+	    System.out.println("Done");
+	}catch(IOException e){
+	    e.printStackTrace();
+	}
+}
+
 }
